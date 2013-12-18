0d1526255a1f2d8462e3aa218d8233aa69af5bfe
GrepRuby <grepruby@GrepRubys-Mac-mini-2.local>
2013-12-18 17:19:57 +0530

    second changes

--- src/com/example/learninggit/MainActivity.java
+++ src/com/example/learninggit/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.learninggit;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,13 +11,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Toast.makeText(getApplicationContext(), "Hello android", 1000).show();
+		
 	}
+	
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	
 
 }
